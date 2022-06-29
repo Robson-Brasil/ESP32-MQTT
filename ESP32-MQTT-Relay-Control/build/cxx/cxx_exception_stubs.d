21457be14fa1e521253143c0b69739ec1931f219
Robson-Brasil <robson.brasil@outlook.com>
2022-06-29 15:01:22 -0400

    Inclusão de um Novo Repositório

--- ESP32-MQTT-Relay-Control/build/cxx/cxx_exception_stubs.d
+++
@@ -1,6 +0,0 @@
-cxx_exception_stubs.o: \
- C:/msys32/home/Alext/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
- C:/msys32/home/Alext/esp/tcp/build/include/sdkconfig.h
-
-C:/msys32/home/Alext/esp/tcp/build/include/sdkconfig.h:
-/home/Alext/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
--- ESP32-MQTT-Relay-Control/build/freemodbus/modbus/functions/mbfuncdiag.d
+++
@@ -1,3 +0,0 @@
-modbus/functions/mbfuncdiag.o: \
- C:/msys32/home/Alext/esp/esp-idf/components/freemodbus/modbus/functions/mbfuncdiag.c
-/home/Alext/esp/esp-idf/components/freemodbus/modbus/functions/mbfuncdiag.c:
